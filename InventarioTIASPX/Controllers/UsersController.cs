344d4a47316b2e368028a9cebca9fef7d9a6ffd1
Angel Jimenez <angel.g.j.reyes@outlook.com>
2020-09-20 16:39:48 -0500

    + Se crea una vicha para visualizar los datos del usuario

--- InventarioTIASPX/Controllers/UsersController.cs
+++ InventarioTIASPX/Controllers/UsersController.cs
@@ -2,6 +2,7 @@
 using InventarioTIASPX.Services;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -11,6 +12,7 @@ namespace InventarioTIASPX.Controllers
     public class UsersController : Controller
     {
         // GET: Users
+        [HttpGet]
         public ActionResult Index()
         {
             ViewData["users"] = RepositoryUser.GetAllUsers();
@@ -18,7 +20,34 @@ namespace InventarioTIASPX.Controllers
         }
 
         [HttpGet]
+        public ActionResult User(long? userId, string msgType, string msgString)
+        {
+            if (userId != null)
+                if (RepositoryUser.Exist(userId.Value))
+                {
+                    ViewData["user"] = RepositoryUser.Get(userId.Value);
+
+                    return View();
+
+                }
+
+            return Redirect(Url.Action("", "Users"));
+        }
+
+        [HttpGet]
         public ActionResult NewUser(string msgType, string msgString)
+        {
+            if (msgType != null && msgString != null)
+            {
+                msgString = Application.ApplicationManager.Base64Decode(msgString);
+                ViewData["message"] = new { msgType, msgString };
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult EditUser(long userId, string msgType, string msgString)
         {
             if (msgType != null && msgString != null)
             {
@@ -43,6 +72,12 @@ namespace InventarioTIASPX.Controllers
                 RepositoryUser.Add(user);
 
             return Redirect(Url.Action("NewUser", "Users", new { msgType = "success", msgString = Application.ApplicationManager.Base64Encode($"El usuario {user.UserId} fue registrado correctamente.") }));
+        }
+
+        [HttpPost]
+        public ActionResult Edit(User user)
+        {
+            return Redirect(Url.Action("User", "Users", new { userId = user.UserId }));
         }
     }
 }
