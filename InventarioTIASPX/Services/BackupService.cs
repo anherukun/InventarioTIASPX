fed07f1e16fb5ed1bc2659674ebc87e0f86769cd
Angel Jimenez <angel.g.j.reyes@outlook.com>
2020-10-03 22:41:37 -0500

    + Cambios visuales

--- InventarioTIASPX/Services/BackupService.cs
+++ InventarioTIASPX/Services/BackupService.cs
@@ -12,6 +12,8 @@ namespace InventarioTIASPX.Services
         // NOTES & FILES PREFIX: _COMP | _USR
         public static void RecoverData(Backup backup)
         {
+            BackupService.ClearCurrentContext();
+
             List<Note> ComputerNotes = new List<Note>();
             List<Note> UserNotes = new List<Note>();
 
@@ -30,20 +32,42 @@ namespace InventarioTIASPX.Services
             foreach (var item in backup.ComputerDeviceRelationship)
                 RepositoryDevice.AssignComputer(item.FirstOrDefault().Key, item.FirstOrDefault().Value);
 
-            // Reconstruye el origen de las notas
+            // Reconstruye el origen de las notas y las inserta en la base de datos
             foreach (var item in backup.Notes)
             {
                 if (item.ParentObjectId.Contains("COMP_"))
-                    ComputerNotes.Add(item);
+                {
+                    item.ParentObjectId = item.ParentObjectId.Replace("COMP_", "");
+                    new RepositoryComputerNotes().Add(item);
+                }
                 if (item.ParentObjectId.Contains("USR_"))
-                    UserNotes.Add(item);
-            }
+                {
+                    item.ParentObjectId = item.ParentObjectId.Replace("USR_", "");
+                    new RepositoryUserNotes().Add(item);
+                }
+            }                
+        }
 
-            // Inserta las entidades dependientes
-            foreach (var item in ComputerNotes) 
-                new RepositoryComputerNotes().Add(item);
-            foreach (var item in UserNotes)
-                new RepositoryUserNotes().Add(item);
+        /// <summary>
+        /// Elimina las entidades del contexto actual
+        /// </summary>
+        private static void ClearCurrentContext()
+        {
+            // Elimina las entidades de Notas
+            foreach (var item in new RepositoryGenericNotes().GetAll())
+                new RepositoryGenericNotes().Delete(item.NoteId);
+
+            // Elimina las entidades de Computadoras
+            foreach (var item in RepositoryComputer.GetAllComputers())
+                RepositoryComputer.Delete(item.ComputerId);
+
+            // Elimina las entidades de Dispositivos
+            foreach (var item in RepositoryDevice.GetAllDevices())
+                RepositoryDevice.Delete(item.DeviceId);
+
+            // Elimina las entidades de Usuarios
+            foreach (var item in RepositoryUser.GetAllUsers())
+                RepositoryUser.Delete(item.UserId);
         }
     }
 }
